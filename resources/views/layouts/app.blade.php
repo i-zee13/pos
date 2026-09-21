d826b3611c0fed3e88c1b5f5b0994d339694b547
i-zee13 <100059891+i-zee13@users.noreply.github.com>
2026-09-22 01:22:00 +0500

    Updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,9 +47,9 @@
   <link href="{{asset('/css/wizard.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{asset('/css/jquery.steps.css')}}" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('/css/selectize.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('/css/style.css?v=7.2')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/css/style.css?v=7.3')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('/css/menu.css?v=6.4')}}" />
-    <link rel="stylesheet" type="text/css" href="{{asset('/css/product.css?v=8.2')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('/css/product.css?v=8.3')}}" />
     <link rel="stylesheet" type="text/css" href="{{asset('/css/invoice-responsive.css?v=1.0')}}" />
     <link rel="stylesheet" type="text/css" href="{{asset('/css/jquery.mCustomScrollbar.css')}}" />
     <link rel="stylesheet" href="{{asset('/css/animate.css')}}">
